9b6a230ba47f5ed4d0e446dfa4fbc31307153bfc
Justin Tew <justintewtew@gmail.com>
2019-07-14 19:33:43 -0700

    Update EventService.js

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-	baseURL: `http://localhost:3000`,
+	baseURL: `https://justintew-json-server.herokuapp.com`,
 	withCredentials: false, // This is the default
 	headers: {
 		Accept: 'application/json',
